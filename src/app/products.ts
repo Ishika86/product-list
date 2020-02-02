33dc499bd90d5c31f05b7eabe6ff5450edaf7ea0
Ishika <ishi86j@gmail.com>
2020-02-02 12:01:08 +0530

    Added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,20 +1,85 @@
 export const products = [
   {
     productId: 0,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'Samsung',
+    price: 1799,
+    image: 'assets/Images/image1.jpeg',
+    description: 'A standard electronic brand that has products in nearly all the electronic items,' +
+      ' Samsung’s laptops are stunning. Samsung laptops are designed with the greatest care that provides' +
+      ' good quality and full HD displays. Samsung products are known with the series name of Notebook 9, Notebook 7,' +
+      ' Notebook 5 and Notebook Odyssey.'
   },
   {
     productId: 1,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Razer',
+    price: 2699,
+    image: 'assets/Images/image2.jpg',
+    description: 'Razer comes up with the unique feature for laptops and one can explore the entire range of gaming laptops' +
+      ' that will be suitable for work and play. The laptops come with best graphics on great display that has the newest ' +
+      'technology comprised within the system.'
   },
   {
     productId: 2,
-    name: 'Phone Standard',
-    price: 299,
+    name: 'Microsoft Laptop',
+    price: 2299,
+    image : 'assets/Images/image3.jpg',
+    description: 'Microsoft Corporation is American based company that develops, manufactures many licensed software ' +
+      'and hardware products. It is the largest producer of personal computers. Microsoft’s Windows 10 ' +
+      'products are built for faster performance, thinner and they are more powerful. It has a lot of features in a single product'
+  },
+  {
+    productId: 3,
+    name: ' Acer Laptop',
+    price: 8299,
+    image : 'assets/Images/image4.jpg',
     description: 'Phone with so many extra features and with a good features'
+  },
+  {
+    productId: 4,
+    name: 'Apple Laptop',
+    price: 5299,
+    image : 'assets/Images/image5.jpg',
+    description: ' Apple produces far-fetched laptop at a marginally higher price.' +
+      ' The prices are based on the size, memory, graphics memory and many features. Apple majorly produces' +
+      ' laptop of 13-inch and 15 inch that can have a larger disk storage and also increase the performance of the laptop.'
+  },
+  {
+    productId: 5,
+    name: ' Asus Laptop',
+    price: 4299,
+    image : 'assets/Images/image6.jpg',
+    description: 'Asus is a computer hardware and electronics company that is based out in Taiwan.' +
+      ' Asus is considered as number one in notebook, motherboard and gaming series. ' +
+      'Asus has to their credit 29 years of technology services, 2511 awards received during the year 2017 ' +
+      'and more than 200 service centers.'
+  },
+  {
+    productId: 6,
+    name: 'Dell Laptop',
+    price: 3299,
+    image : 'assets/Images/image7.jpg',
+    description: 'Dell is an American company that sells laptops, computers, data storage, servers and computer peripherals.' +
+      ' Dell laptops are configured with all the latest technology to meet the meeting demands of every business and to handle any task.' +
+      ' Their famous laptops brands are Inspiron-Everyday Essentials, XPS-Performance, Vostro and Alienware laptops.'
+  },
+  {
+    productId: 7,
+    name: ' Lenovo Laptop',
+    price: 5299,
+    image : 'assets/Images/image8.jpg',
+    description: ' Lenovo laptop comes with laptops based on priorities.' +
+      ' Performance laptops are designed for professionals, gamers, and creators. ' +
+      'Ultraportable laptops are used by people who carry laptops wherever they go.' +
+      'Budget laptops are used for the home purpose which provides value and performance in a lone package.' +
+      ' Innovative 2 in 1 laptops are used as laptop and tablet.'
+  },
+  {
+    productId: 8,
+    name: 'Hewlett Packard Laptop',
+    price: 8299,
+    image : 'assets/Images/image9.png',
+    description: 'Hewlett Packard (HP) is the world’s largest company in electronics and computer.' +
+      ' The laptops provided by HP are fully loaded with all the features that the users are looking into.' +
+      ' The 2 in 1 laptops of HP are thin and light. It provides good battery life to the users.'
   }
 ];
